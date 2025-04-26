2c1b6bcdc0ca431e3150228d6769a16684dc13b1
Mrugesh Thesiya <mrugeshthesiya16@gmail.com>
2025-04-26 15:43:33 +0530

    updated merge sort

--- Prac 01 and 02/Sorting/mergeSort.c
+++ Prac 01 and 02/Sorting/mergeSort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 void printArray(int* a,int size){
  for (int i = 0 ; i < size ; i++){
@@ -7,26 +8,61 @@ void printArray(int* a,int size){
  printf("\n");
 }
 
-void mergeSort(int* a,int l,int r){
-if(l<r){
-  int m = (l+r)/2;
+void merge(int* a,int l,int m,int r){
+  int n1 = m-l+1;
+  int n2 = r-m;
   
-  mergeSort(a,l,m);
-  mergeSort(a,m+1,r);
+  int L[n1+1];
+  int R[n2+1];
   
-  merge(a,l,m,r);
+  for(int i = 0 ; i < n1 ; i++){
+    L[i] = a[l+i];
+  }
+  
+  L[n1] = INT_MAX;
+  
+  for(int j = 0 ; j < n2 ; j++){
+    R[j] = a[m+j+1];
+  }
+  
+  R[n2] = INT_MAX;
+  
+  int i = 0 , j = 0,k = l;
+  while(k<=r){
+  
+    if(L[i]<R[j]){
+    
+      a[k++]=L[i++];
+      
+    } else{
+    
+      a[k++]=R[j++];
+      
+    }
+  }
 }
+
+void mergeSort(int* a,int l,int r){
+  if(l<r){
+    int m = (l+r)/2;
+  
+    mergeSort(a,l,m);
+    mergeSort(a,m+1,r);
+  
+    merge(a,l,m,r);
+  }
 }
 
 int main(){
-int a[5] = {23,15,46,25,16};
+int a[] = {10,9,8,7,6,5,4,3,2,1};
+int size = sizeof(a)/sizeof(a[0]);
 printf("Initially : ");
-printArray(a,5);
+printArray(a,size);
 
-insertionSortShifting(a,5);
+mergeSort(a,0,size-1);
 
 printf("After Sorting : ");
-printArray(a,5);
+printArray(a,size);
 
 return 0;
 }
