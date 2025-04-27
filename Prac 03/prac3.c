b88157244f964574752fe24d7f9a2d64c10e2f7d
Mrugesh Thesiya <mrugeshthesiya16@gmail.com>
2025-04-27 15:22:07 +0530

    update

--- Prac 03/prac3.c
+++ Prac 03/prac3.c
@@ -14,7 +14,7 @@ Node* createNode(int value){
   return newNode;
 }
 
-void addNodeAtEnd(Node** head,int value){
+void addNodeAtStart(Node** head,int value){
   Node* newNode = createNode(value);
   
   if(*head == NULL){
@@ -23,11 +23,8 @@ void addNodeAtEnd(Node** head,int value){
   }
   
   Node* temp = *head;
-  
-  while(temp->next != NULL){
-    temp = temp->next;
-  }
-  temp->next = newNode;
+  newNode->next = temp;
+  *head = newNode;
 }
 
 void PrintNumber(Node* head){
@@ -38,14 +35,34 @@ void PrintNumber(Node* head){
   }
 }
 
-Node* takeInput(int number,int size){
+void PrintNumberReverse(Node* head){
+  Node* temp1 = head;
+  Node* temp2 = NULL;
+  while(temp1 != NULL){
+    addNodeAtStart(&temp2,temp1->value);
+    temp1 = temp1->next;
+  }
+  while(temp2 != NULL){
+    printf("%d",temp2->value);
+    temp2 = temp2->next;
+  }
+}
+
+Node* takeInput(int number){
   Node* head = NULL;
   int i = 1;
-  while(i<=size){
+  while(true){
     int value;
-    printf("Enter the digit %d of number %d: ",i++,number);
+    printf("Enter the digit %d of number %d or enter 10 to exit : ",i++,number);
     scanf("%d",&value);
-    addNodeAtEnd(&head,value);
+    if( value == 10 ){
+      break;
+    }
+    if((value / 10) > 0){
+        printf("You can only enter a single digit at a time. \n");
+        i--;
+    }
+    addNodeAtStart(&head,value);
   }
   return head;
 }
@@ -54,35 +71,24 @@ void add(Node* n1,Node* n2){
    Node* head = NULL;
    Node* temp1 = n1;
    Node* temp2 = n2;
-   while(temp1 != NULL){
-    int value = temp1->value + temp2->value;
-    addNodeAtEnd(&head,value);
-    temp1 = temp1->next;
-    temp2 = temp2->next;
+   int carry = 0;
+   while(temp1 != NULL || temp2 != NULL || carry != 0){
+    int val1 = temp1 != NULL ? temp1->value : 0;
+    int val2 = temp2 != NULL ? temp2->value : 0;
+    
+    int sum = val1 + val2 + carry;
+    carry = sum / 10;
+    int digit = sum % 10;
+    
+    addNodeAtStart(&head,digit);
+    
+    if(temp1!=NULL) temp1 = temp1->next;
+    if(temp2!=NULL) temp2 = temp2->next;
    }
    printf("The Sum of ");
-   PrintNumber(n1);
+   PrintNumberReverse(n1);
    printf(" and ");
-   PrintNumber(n2);
-   printf(" is ");
-   PrintNumber(head);
-   printf(".");
-}
-
-void sub(Node* n1,Node* n2){
-   Node* head = NULL;
-   Node* temp1 = n1;
-   Node* temp2 = n2;
-   while(temp1 != NULL){
-    int value = temp1->value - temp2->value;
-    addNodeAtEnd(&head,value);
-    temp1 = temp1->next;
-    temp2 = temp2->next;
-   }
-   printf("The Diff of ");
-   PrintNumber(n1);
-   printf(" and ");
-   PrintNumber(n2);
+   PrintNumberReverse(n2);
    printf(" is ");
    PrintNumber(head);
    printf(".");
@@ -90,12 +96,25 @@ void sub(Node* n1,Node* n2){
 
 
 int main(){
-  int size = 3;
-  Node* n1 = takeInput(1,size);
-  Node* n2 = takeInput(2,size);
+  Node* n1 = takeInput(1);
+  Node* n2 = takeInput(2);
   
-  //add(n1,n2);
-  sub(n1,n2);
+  printf("\n--- ADDITION ---\n");
+  add(n1, n2);
+  /*
+  
+  printf("\n--- SUBTRACTION ---\n");
+  sub(n1, n2);
+
+  printf("\n--- MULTIPLICATION ---\n");
+  mul(n1, n2);
+
+  printf("\n--- EXPONENTIATION ---\n");
+  int exponent;
+  printf("Enter exponent for first number: ");
+  scanf("%d", &exponent);
+  exp_(n1, exponent)
+  */
   
   return 0;
 }
